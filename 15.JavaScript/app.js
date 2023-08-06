9591f9150459877a783ddd4bae573fc0f5749969
VineethaJNV <90920260+VineethaJNV@users.noreply.github.com>
2023-08-06 17:15:49 +0530

    Java Script as a programming language - part 2
    Java Script as a programming language - part 2

---
+++ 15.JavaScript/app.js
@@ -0,0 +1,14 @@
+let msg = "     Hello     ";
+
+// let newMsg = "   he       ll    o "
+newMsg = msg.trim();
+newMsg = newMsg.toUpperCase();
+console.log(newMsg);
+
+
+// let password = prompt("Set your password");
+// console.log(password.trim());
+
+let name = "Apna College";
+
+let temp  = "IloveCoding";
