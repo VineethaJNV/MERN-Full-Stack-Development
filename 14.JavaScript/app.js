5d935759585efea180e314f7651b75411d244376
VineethaJNV <90920260+VineethaJNV@users.noreply.github.com>
2023-08-06 17:15:17 +0530

    Java Script as a programming language - part 1
    Java Script as a programming language - part 1

---
+++ 14.JavaScript/app.js
@@ -0,0 +1,54 @@
+//Practise Question
+let color = "red";
+if(color === "red"){
+    console.log("stop!");
+}
+if(color==="yellow"){
+    console.log("slow down");
+}
+if(color == "green"){
+    console.log("go");
+}
+
+//Arithmetic operators
+let a = 10;
+let b = 5;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+
+//Unary Operators
+console.log(a++);
+console.log(++a); 
+//Assignment operatora
+b = a;
+console.log(b);
+
+//Comparision
+
+let age = 18;
+console.log(age<18);
+console.log(5>6);
+
+//Conditional Statements
+let firstName = "Vineetha"
+if(firstName == "Vineetha"){
+    console.log(`Welcome ${firstName}`);
+}
+
+
+
+
+// console.log("Hello World!")
+// let a = 10;
+// let b = 5;
+// console.log("sum is : ", a+b);
+
+// let pencil = 10;
+// let eraser = 5;
+// // let output = "the total price is: "+(pencil + eraser)+ "Rupees."
+// let output = `the total price is: ${pencil + eraser}Rupees`
+// console.log(output)
